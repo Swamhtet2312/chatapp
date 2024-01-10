cb595d9fffdccbadbf1c803ebbf4363868181810
Swamhtet2312 <88706068+Swamhtet2312@users.noreply.github.com>
2024-01-10 16:44:34 +0630

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
-import streamlit as st
 import random
 from string import ascii_uppercase
 
